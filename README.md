30adbaaab46e7a5af284c6b7e5c40a7677f64627
Anastasiia <egoinitium@gmail.com>
2026-02-03 00:51:09 +0300

    update - add spot the differences

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/illiarem/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/illiarem/frontend-project-46/actions)
+
+## example
+[![asciicast](https://asciinema.org/a/nWdYfWywHoZUrsJd.svg)](https://asciinema.org/a/nWdYfWywHoZUrsJd)
--- gendiff.js
+++ gendiff.js
@@ -2,6 +2,7 @@
 
 import { program } from 'commander'
 import parseDate from './dataParsing.js'
+import _ from 'lodash'
 
 program
   .version('0.0.1')
@@ -9,7 +10,25 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    parseDate(filepath1, filepath2)
+    const [obj1, obj2] = parseDate(filepath1, filepath2)
+
+    const sortedKeys = _.sortBy([...new Set([...Object.keys(obj1), ...Object.keys(obj2)])])
+    const result = sortedKeys.map((key) => {
+      if (!Object.hasOwn(obj2, key)) {
+        return `  - ${key}: ${obj1[key]}`
+      }
+      else if (!Object.hasOwn(obj1, key)) {
+        return `  + ${key}: ${obj2[key]}`
+      }
+      else if (obj1[key] !== obj2[key]) {
+        return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`
+      }
+      else {
+        return `    ${key}: ${obj1[key]}`
+      }
+    })
+
+    console.log(`{\n${result.join('\n')}\n}`)
   })
 
 program.parse()
