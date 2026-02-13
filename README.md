6c7742ead292bb7a47eeb0e3266157df66d65705
Anastasiia <egoinitium@gmail.com>
2026-02-14 01:27:42 +0300

    update

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=illiarem_frontend-project-46&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=illiarem_frontend-project-46)
 
 ## example
-[![asciicast](https://asciinema.org/a/zfhH1rOQEXV272U3.svg)](https://asciinema.org/a/zfhH1rOQEXV272U3)
+[![asciicast](https://asciinema.org/a/idsTW6KLHTbsq4Mj.svg)](https://asciinema.org/a/idsTW6KLHTbsq4Mj)
--- src/comparison.js
+++ src/comparison.js
@@ -1,20 +1,32 @@
 import _ from 'lodash'
 
 export const compare = (obj1, obj2) => {
-  const sortedKeys = _.sortBy([...new Set([...Object.keys(obj1), ...Object.keys(obj2)])])
+  const sortedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
   const result = sortedKeys.map((key) => {
+    const node = {}
+    node.key = key
     if (!Object.hasOwn(obj2, key)) {
-      return `  - ${key}: ${obj1[key]}`
+      node.type = 'deleted'
+      node.value = obj1[key]
     }
     else if (!Object.hasOwn(obj1, key)) {
-      return `  + ${key}: ${obj2[key]}`
+      node.type = 'added'
+      node.value = obj2[key]
+    }
+    else if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      node.type = 'object inside'
+      node.children = compare(obj1[key], obj2[key])
     }
     else if (obj1[key] === obj2[key]) {
-      return `    ${key}: ${obj1[key]}`
+      node.type = 'unchanged'
+      node.value = obj1[key]
     }
     else {
-      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`
+      node.type = 'changed'
+      node.oldValue = obj1[key]
+      node.newValue = obj2[key]
     }
+    return node
   })
-  return `{\n${result.join('\n')}\n}`
+  return result
 }
